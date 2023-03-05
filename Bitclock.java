0c19a8bad79677479db3ecd9c43a297dd14de42f
Matt <ernstm1@go.stockton.edu>
2023-03-04 20:25:20 -0500

    It's a binary clock!

--- Bitclock.java
+++ Bitclock.java
@@ -2,62 +2,85 @@
  * @author Matthew Ernst
  */
 
+import java.time.LocalDateTime;
 public class Bitclock{
 
-    private String merid, hour, min;
+    private String merids, hours, mins;
+    private String[][] clock = new String[3][6];
 
-    public Bitclock(int merid, int hour, int min){
-        this.merid = Integer.toBinaryString(merid);
-        this.hour = Integer.toBinaryString(hour);
-        this.min = Integer.toBinaryString(min);
-    }
+    public Bitclock(){
+        hours = Integer.toBinaryString(LocalDateTime.now().getHour());
+        mins = Integer.toBinaryString(LocalDateTime.now().getMinute());
 
+        if(LocalDateTime.now().getHour()/12 > 0){
+            hours = Integer.toBinaryString(LocalDateTime.now().getHour() %12);
+            merids = "1";
+        }
 
-
-    public String toString(){
-        String[][] ret = new String[3][6];
-        final int column = 6, row = 3;
+        //instantiates the merid column (AM(0) or PM(1))
         int count = 1;
+        for (int i = 5; i >= 0; i--) {
+            if (count <= merids.length())
+                clock[0][i] = String.valueOf(merids.charAt(merids.length() - count));
+            else clock[0][i] = "0";
 
-        String hours, minutes, merids;
-        hours = this.getHour();
-        minutes = this.getMin();
-        merids = this.getMerid();
-
-        //instantiates the merid
-        ret[0][5] = this.getMerid();
-
-        //Instantiates the minute column
-        for(int i = column - 1; i >= 0; i--){
-            ret[1][i] = String.valueOf(hours.charAt(hours.length() - count));
-            System.out.println(ret[1][i]);
             count++;
         }
 
-        for(int i = 5; i >= 0; i--){
-            System.out.println(ret[1][i]);
+        //Instantiates the hours column
+        count = 1;
+        for (int i = 5; i >= 0; i--) {
+            if (count <= hours.length())
+                clock[1][i] = String.valueOf(hours.charAt(hours.length() - count));
+            else clock[1][i] = "0";
+
+            count++;
         }
-        return ret.toString();
+
+        //Instantiates the minutes column
+        count = 1;
+        for (int i = 5; i >= 0; i--) {
+            if (count <= mins.length())
+                clock[2][i] = String.valueOf(mins.charAt(mins.length() - count));
+
+            else clock[2][i] = "0";
+
+            count++;
+        }
+
+
     }
 
+    public String toString(){
+        String complete = "";
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 6; j++)
+                complete = complete + clock[i][j];
+
+            complete = complete + "\n";
+        }
+
+        return complete;
+    }
+
+    public String[][] getRet() {
+        return clock;
+    }
     public String getMerid() {
-        return merid;
+        return merids;
     }
 
     public String getHour() {
-        return hour;
+        return hours;
     }
 
     public String getMin() {
-        return min;
-    }
-
-    public static String convert(int nums){
-        return Integer.toBinaryString(nums);
+        return mins;
     }
 
     public static void main(String[] args){
-        Bitclock btc = new Bitclock(1, 5, 30);
+        Bitclock btc = new Bitclock();
         System.out.println(btc);
     }
 }
